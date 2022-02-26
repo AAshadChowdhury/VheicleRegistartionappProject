661c3a3f5cabc1a915864a420a4a9f3ca5e5071e
ALI ASHAD ULLAH <ALI ASHAD ULLAH@ALI-ASHAD-ULLAH>
2022-02-27 01:17:02 +0600

    Add project files.

---
+++ MonthlyProject/Vehicles.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonthlyProject
+{
+    public class Vehicles : INotifyPropertyChanged
+    {
+        public String RegNo { get; set; }
+        public string _regdOwner;
+        public string RegdOwner
+        {
+            get => this._regdOwner;
+            set
+            {
+                this._regdOwner = value;
+                this.OnPropertyChanged(nameof(RegdOwner));
+            }
+        }
+        public string _address;
+        public string Address
+        {
+            get => this._address;
+            set
+            {
+                this._address = value;
+                this.OnPropertyChanged(nameof(Address));
+            }
+        }
+        public string _makersClass;
+        public string MakersClass
+        {
+            get => this._makersClass;
+            set
+            {
+                this._makersClass = value;
+                this.OnPropertyChanged(nameof(MakersClass));
+            }
+        }
+        public string _vehiclesClass;
+        public string VehiclesClass
+        {
+            get => this._vehiclesClass;
+            set
+            {
+                this._vehiclesClass = value;
+                this.OnPropertyChanged(nameof(VehiclesClass));
+            }
+        }
+
+        public string _fuelUsed;
+        public string FuelUsed
+        {
+            get => this._fuelUsed;
+            set
+            {
+                this._fuelUsed = value;
+                this.OnPropertyChanged(nameof(FuelUsed));
+            }
+        }
+        public string _engineCC;
+        public string EngineCC
+        {
+            get => this._engineCC;
+            set
+            {
+                this._engineCC = value;
+                this.OnPropertyChanged(nameof(EngineCC));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,
+                new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+}
---
+++ MonthlyProject/ViewModel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace MonthlyProject
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private List<Vehicles> V;
+        private int currentCustomer;
+        public Command NextCustomer { get; private set; }
+        public Command PreviousCustomer { get; private set; }
+        public ViewModel()
+        {
+            currentCustomer = 0;
+            this.IsAtStart = true;
+            this.IsAtEnd = false;
+            this.NextCustomer = new Command(this.Next, () =>
+this.V.Count > 1 && !this.IsAtEnd);
+            this.PreviousCustomer = new Command(this.Previous, () =>
+            this.V.Count > 0 && !this.IsAtStart);
+            this.V = new List<Vehicles>();
+            string fileName = "TextFile1.txt";
+            try
+            {
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var l = line.Split(",");
+                        V.Add(new Vehicles() { RegNo = l[0], RegdOwner = l[1], Address = l[2], MakersClass = l[3], VehiclesClass = l[4], FuelUsed = l[5], EngineCC = l[6] });
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageDialog a = new MessageDialog(exp.Message);
+                a.ShowAsync();
+            }
+
+        }
+        public Vehicles Current
+        {
+            get => this.V.Count > 0 ? this.V[currentCustomer] :
+            null;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,
+                new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        private bool _isAtStart;
+        public bool IsAtStart
+        {
+            get => this._isAtStart;
+            set
+            {
+                this._isAtStart = value;
+                this.OnPropertyChanged(nameof(IsAtStart));
+            }
+        }
+        private bool _isAtEnd;
+        public bool IsAtEnd
+        {
+            get => this._isAtEnd;
+            set
+            {
+                this._isAtEnd = value;
+                this.OnPropertyChanged(nameof(IsAtEnd));
+            }
+        }
+        private void Next()
+        {
+            if (this.V.Count - 1 > this.currentCustomer)
+            {
+                this.currentCustomer++;
+                this.OnPropertyChanged(nameof(Current));
+                this.IsAtStart = false;
+                this.IsAtEnd = (this.V.Count - 1 == this.currentCustomer);
+            }
+        }
+        private void Previous()
+        {
+            if (this.currentCustomer > 0)
+            {
+                this.currentCustomer--;
+                this.OnPropertyChanged(nameof(Current));
+                this.IsAtEnd = false;
+                this.IsAtStart = (this.currentCustomer == 0);
+            }
+
+        }
+    }
+
+}
